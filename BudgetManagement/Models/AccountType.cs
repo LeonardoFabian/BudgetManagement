1340e160fe13c01f9b99bbc090e747f5c3ab988f
Leonardo Fabian <ramonlfabian@gmail.com>
2022-09-18 22:00:58 -0400

    Account type form validations

--- BudgetManagement/Models/AccountType.cs
+++ BudgetManagement/Models/AccountType.cs
@@ -1,10 +1,19 @@
-﻿namespace BudgetManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetManagement.Models
 {
     public class AccountType
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "the length of the {0} field must be between {2} and {1} characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required")]
+        [Display(Name = "Order Number")]
         public int OrderNumber { get; set; }
+
         public int UserId { get; set; }
     }
 }
