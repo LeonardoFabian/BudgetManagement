e2a5ee18775292b653c76c2cb6c400a007ff8833
Leonardo Fabian <ramonlfabian@gmail.com>
2022-09-18 20:28:37 -0400

    Create Account Type form

---
+++ BudgetManagement/Controllers/AccountTypeController.cs
@@ -0,0 +1,19 @@
+﻿using BudgetManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManagement.Controllers
+{
+    public class AccountTypeController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create( AccountType accountType )
+        {
+            return View();
+        }
+    }
+}
---
+++ BudgetManagement/Models/AccountType.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetManagement.Models
+{
+    public class AccountType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OrderNumber { get; set; }
+        public int UserId { get; set; }
+    }
+}
